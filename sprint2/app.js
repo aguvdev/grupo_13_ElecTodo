6409b6f02de9a455064d90dbca29db46681efba1
hannagustinvega <hannheitiragustin@hotmail.com>
2021-06-30 02:04:09 -0300

    primer commit

--- sprint2/app.js
+++ sprint2/app.js
@@ -5,4 +5,6 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')))
+
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
