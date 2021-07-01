4b12892809f71ed323686e0fb287a50767727ce1
rorecalde <rorecalde3@gmail.com>
2021-07-01 16:43:07 -0300

    subo html y css del carrito

--- sprint2/app.js
+++ sprint2/app.js
@@ -6,3 +6,6 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
+app.get('/CarritoDeCompras', (req, res) => res.sendFile(path.join(__dirname, 'views', 'carrito.html')))
+app.get('/MetodoDePago', (req, res) => res.sendFile(path.join(__dirname, 'views', 'metodoDePago.html')))
+app.get('/ConfirmacionDeLaCompra', (req, res) => res.sendFile(path.join(__dirname, 'views', 'confirmacion.html')))
