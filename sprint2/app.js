4f0599985af76aa156c829e9479e6bc15d78ea78
hannagustinvega <hannheitiragustin@hotmail.com>
2021-07-02 02:22:56 -0300

    beta final de login

--- sprint2/app.js
+++ sprint2/app.js
@@ -5,4 +5,6 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')))
+
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
