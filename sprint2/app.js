a459e20c3ab28245bb7cf2e17ab4b4fc4e762a1b
PatricioVill <patricio.vill74@gmail.com>
2021-07-04 16:28:45 -0300

    Aporto carga de producto

--- sprint2/app.js
+++ sprint2/app.js
@@ -4,5 +4,6 @@ const path = require('path');
 const port = 3000;
 
 app.use(express.static('public'));
+app.get('/carga',(req,res) => res.sendFile(path.join(__dirname,'views','carga.html')))
 
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
