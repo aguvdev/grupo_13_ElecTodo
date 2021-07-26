3302e6c895ee1bd4749cdb6063c3be76cdadc50b
rorecalde <rorecalde3@gmail.com>
2021-07-26 17:06:50 -0300

    elimino un comentario

--- sitio/app.js
+++ sitio/app.js
@@ -22,10 +22,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/cart', cartRouter);
-//app.use('/product', productRouter);
+app.use('/product', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
