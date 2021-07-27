a6c4f5082df4ee2b038f6222e267178d2220c3b2
hannagustinvega <hannheitiragustin@hotmail.com>
2021-07-27 15:21:15 -0300

    estructura ejs

--- sitio/app.js
+++ sitio/app.js
@@ -23,9 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/cart', cartRouter);
-app.use('/product', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
