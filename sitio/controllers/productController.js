3cb6a0d1c67c61aae7a51230fefde01f027e7445
sergioromeroo <manusergio96@gmail.com>
2021-08-11 03:15:04 -0300

    genero los campos para producto y genero json

--- sitio/controllers/productController.js
+++ sitio/controllers/productController.js
@@ -1,7 +1,17 @@
+const productos = require("../data/products_db");
+const description = require("../data/description_db");
+const relacionados = require("../data/relacionados_db");
+
 module.exports={
     product : (req,res) => {
+        
         return res.render("product",{
+            productos,
+            description,
+            relacionados
             
         })
     }
+
+    
 }
