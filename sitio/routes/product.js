db132de350bcb5eb113c4cbbe960c1f4d5080a81
sergioromeroo <manusergio96@gmail.com>
2021-08-11 17:56:39 -0300

    cambio deatail por product

--- sitio/routes/product.js
+++ sitio/routes/product.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const {detail} = require("../controllers/productController")
+const {product} = require("../controllers/productController")
 
 /* GET products listing. */
 router.get("/detalle-product/:id", product);
