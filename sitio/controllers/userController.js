8b154ee396cb43ec02e359a910ba130749f16264
rorecalde <rorecalde3@gmail.com>
2021-08-17 20:49:02 -0300

    validaciones login y register

--- sitio/controllers/userController.js
+++ sitio/controllers/userController.js
@@ -1,16 +1,30 @@
 const {usuarios, guardar} = require('../data/user_db');
+const {validationResult} = require('express-validator');
 
 module.exports = {
     login : (req,res) => {
         return res.render('login')
     },
     processLogin : (req,res) => {
-        return res.redirect('/')
+        const result = validationResult(req);
+        if(result.errors.length > 0){
+            return res.render('login', {
+                errors: result.mapped(),
+                oldData: req.body
+            })
+        }
     },
     register : (req,res) => {
         return res.render('register')
     },
     processRegister : (req,res) => {
+        const result = validationResult(req);
+        if(result.errors.length > 0){
+            return res.render('register', {
+                errors: result.mapped(),
+                oldData: req.body
+            })
+        }
         return res.redirect('/')
-    },
+    }
 }
--- sitio/routes/users.js
+++ sitio/routes/users.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+
+const {loginValidation, registerValidation} = require('../validations/usersValidation')
 const {login,register, processLogin, processRegister} = require('../controllers/userController')
 
 /* GET /users listing. */
 router.get('/login', login);
-router.post('/login', processLogin)
+router.post('/login',loginValidation, processLogin)
 
 router.get('/register', register);
-router.post('/register', processRegister);
+router.post('/register', registerValidation, processRegister);
 
 module.exports = router;
