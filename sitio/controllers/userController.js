8fb93891f93d9e659573d7837aae078ddc18ca08
hannagustinvega <hannheitiragustin@hotmail.com>
2021-08-27 17:47:27 -0300

    muchas cosas

--- sitio/controllers/userController.js
+++ sitio/controllers/userController.js
@@ -1,33 +1,86 @@
 const {usuarios, guardar} = require('../data/user_db');
 const {validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
     login : (req,res) => {
-        return res.render('login')
+        return res.render('../views/users/login', {
+            usuarios
+        })
     },
     processLogin : (req,res) => {
         const result = validationResult(req);
-        if(result.errors.length > 0){
-            return res.render('login', {
+        const {email, recordar} = req.body
+        if(result.isEmpty()){
+            let usuario = usuarios.find(usuario => usuario.email === email)
+            req.session.userLogin = {
+                nombre : usuario.nombre,
+                rol : usuario.rol
+            }
+            res.locals.userLogin = req.session.userLogin
+            if(recordar){
+                res.cookie('elecTodo',req.session.userLogin,{maxAge: 1000 * 60})
+                req.session.userLogin = req.cookies.elecTodo
+            }
+            return res.redirect('/')
+        }else{
+            return res.render('../views/users/login', {
                 errors: result.mapped(),
                 oldData: req.body
             })
         }
     },
     register : (req,res) => {
-        return res.render('register')
+        return res.render('../views/users/register')
     },
     processRegister : (req,res) => {
         const result = validationResult(req);
-        if(result.errors.length > 0){
-            return res.render('register', {
+        
+        let emailRegistrado = usuarios.find(usuario => usuario.email === req.body.email)
+        if(emailRegistrado){
+            return res.render('../views/users/register', {
+                errors: {
+					email: {
+						msg: 'Este email ya está registrado'
+					}
+				},
+				oldData: req.body
+            })
+        }
+
+        if(result.isEmpty()){  
+            let {nombre, lastname, email}= req.body
+            let newUser = {
+                userId : usuarios.length > 0 ? usuarios[usuarios.length - 1].userId + 1 : 1,
+                nombre,
+                lastname,
+                email,
+                password : bcrypt.hashSync(req.body.password, 10),
+                image : req.file ? req.file.filename : 'foto-de-perfil-default.png',
+                rol : "user"
+            }
+            usuarios.push(newUser);
+            guardar(usuarios);
+
+            req.session.userLogin = {
+                userId : newUser.userId,
+                nombre : newUser.nombre
+            }
+
+            return res.redirect('/')
+        }else{
+            return res.render('../views/users/register', {
                 errors: result.mapped(),
                 oldData: req.body
             })
         }
+    },
+    logout : (req,res) =>{
+        req.session.destroy();
+        res.cookie('elecTodo',null,{maxAge:-1})
         return res.redirect('/')
     },
     profile: (req,res) => {
-        return res.render('profile')
+        return res.render('../views/users/profile')
     }
 }
