e05e2c3227707b2c2390761294e4ca3b0700e41a
hannagustinvega <hannheitiragustin@hotmail.com>
2021-10-15 16:49:54 -0300

    nueva vista de perfil

--- sitio/database/config/config.js
+++ sitio/database/config/config.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: "root",
-    password: "mamaflorciro",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "elecTodo_db",
     host: "127.0.0.1",
     dialect: "mysql"
--- sitio/public/javascript/passOculta.js
+++ sitio/public/javascript/passOculta.js
@@ -1,3 +1,16 @@
-var passOculta = document.getElementById('passOculta');
-
-passOculta.innerHTML = "*****";
+const passwordEl = document.querySelector(".password");
+const eyeButton = document.querySelector(".fa");
+let isPass = true;
+function togglePass() {
+    if (isPass) {
+        passwordEl.setAttribute("type", "text");
+        eyeButton.classList.remove("fa-eye-slash");
+        eyeButton.classList.add("fa-eye");
+        isPass = false;
+    } else {
+        passwordEl.setAttribute("type", "password");
+        eyeButton.classList.remove("fa-eye");
+        eyeButton.classList.add("fa-eye-slash");
+        isPass = true;
+    }
+}
